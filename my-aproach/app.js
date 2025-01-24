e87a397ac032577ceb2653e40480b58fbab3d4a0
Jorge <J0rgeMG@live.com>
2025-01-23 18:43:31 -0600

    started with the logic of the gameplay

--- my-aproach/app.js
+++ my-aproach/app.js
@@ -1,16 +1,35 @@
 const newGameButtonElement = document.getElementById("new-game-button");
 
+function startGame(boardArray){
+    let gameOver = false;
+    let player1Turn = true;
+    let playerSymbol = "X";
+
+    while(!gameOver){
+        if(player1Turn){
+
+        }else{
+
+        }
+
+    }
+}
+
 function newGame(){
+    //select elements needed
     const boardElment = document.getElementById("game-board-section");
     const squaresBoardArray = document.querySelectorAll("#game-board li");
 
     //clears the board
-    for(square of squaresBoardArray){
+    for (square of squaresBoardArray) {
         square.innerHTML = "&nbsp;";
     }
 
     //shows the board
     boardElment.classList.remove("hidden");
+
+    //starts the gameplay
+    startGame(squaresBoardArray);
 }
 
 newGameButtonElement.addEventListener("click", newGame);
