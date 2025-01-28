0960bd45570d745e054cd4a65c73816b84bfc0b0
Jorge <J0rgeMG@live.com>
2025-01-28 17:15:14 -0600

    added the logic to place symbols and have turns

--- course-aproach/scripts/app.js
+++ course-aproach/scripts/app.js
@@ -1,4 +1,5 @@
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
     {
@@ -11,18 +12,22 @@ const players = [
     },
 ];
 
+// Modal elements
 const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
-
 const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-errors");
 
+// Button elements
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
 const startNewGameBtnElement = document.getElementById("start-game-btn");
 
+// Game board elements
 const gameAreaElement = document.getElementById("active-game");
+const gameFieldElements = document.querySelectorAll("#game-board li");
+
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
@@ -33,3 +38,7 @@ backdropElement.addEventListener("click", closePlayerConfig);
 formElement.addEventListener("submit", savePlayerConfig);
 
 startNewGameBtnElement.addEventListener("click", startNewGame);
+
+for(const gameFieldElement of gameFieldElements) {
+    gameFieldElement.addEventListener("click", selectGameField);
+}
--- course-aproach/scripts/game.js
+++ course-aproach/scripts/game.js
@@ -6,3 +6,17 @@ function startNewGame(){
 
     gameAreaElement.style.display = "block";
 }
+
+function switchPlayer() {
+    if (activePlayer === 0) {
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+}
+
+function selectGameField(event) {
+    event.target.textContent = players[activePlayer].symbol; //active player changes every turn
+    event.target.classList.add("disabled");
+    switchPlayer();
+}
