ccd62365f0d1808c9b972eb27838944c1f536833
Jorge <J0rgeMG@live.com>
2025-01-27 19:00:47 -0600

    added styling to the error

--- course-aproach/scripts/config.js
+++ course-aproach/scripts/config.js
@@ -6,6 +6,8 @@ function openPlayerConfig() {
 function closePlayerConfig() {
     playerConfigOverlayElement.style.display = "none";
     backdropElement.style.display = "none";
+    formElement.firstElementChild.classList.remove("error");
+    errorsOutputElement.textContent = "";
 }
 
 function savePlayerConfig(event) {
@@ -17,6 +19,7 @@ function savePlayerConfig(event) {
 
     // console.log(enteredPlayerName);
     if (!enteredPlayerName) {
+        event.target.firstElementChild.classList.add("error");
         errorsOutputElement.textContent = "Please enter a valid name!";
         return;
     }
