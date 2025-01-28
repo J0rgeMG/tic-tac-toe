a031ebfeae1137d99f8fa50d8b8b82c4d1b8f759
Jorge <J0rgeMG@live.com>
2025-01-27 18:54:11 -0600

    added logic to enter a name and get an error if entered an empty name

--- course-aproach/scripts/config.js
+++ course-aproach/scripts/config.js
@@ -10,4 +10,14 @@ function closePlayerConfig() {
 
 function savePlayerConfig(event) {
     event.preventDefault();
+
+    const formData = new FormData(event.target);
+    // trim gets ride of extra whitespace in both sides of a string
+    const enteredPlayerName = formData.get("playername").trim();
+
+    // console.log(enteredPlayerName);
+    if (!enteredPlayerName) {
+        errorsOutputElement.textContent = "Please enter a valid name!";
+        return;
+    }
 }
